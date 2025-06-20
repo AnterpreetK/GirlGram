383c1081c353c3bf4c527e4c382abe2d1a7a1665
AnterpreetK <kanterpreet09@gmail.com>
2025-06-20 18:23:31 +0530

    basic working

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,18 +4,16 @@ export default function Layout(props) {
 
     const header = (
         <header>
-            <h1 className="text-gradient">The Brogram</h1>
+            <h1 className="text-gradient">The GirlGram</h1>
             <p><strong>The 30 Simple Workouts Program</strong></p>
         </header>
     )
 
     const footer = (
         <footer>
-            <p>Built by <a href="https://www.smoljames.com" target="_blank">Smoljames</a><br />Styled with <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a></p>
+            <p>Built by <a href="https://github.com/AnterpreetK/GirlGram" target="_blank">Anterpreet Kaur</a><br />Styled with <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a></p>
         </footer>
     )
-
-    //https://www,YOUR_USERNAME.netlify.app
 
     return (
         <>
